b28d92915b01d0509ba6022ff8de322b7a344709
Ilwoong Kim <dldnd67@gmail.com>
2020-10-02 23:49:29 +0900

    NO-ISSUE add prod setting

--- web/django_server/settings.py
+++ web/django_server/settings.py
@@ -14,6 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DEVELOPMENT = 'development'
+ENV = os.environ.get('ENV', DEVELOPMENT).lower()
+
+def is_dev():
+    return ENV.lower() == DEVELOPMENT
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9=@3d2qpct#uk&if198m&)**ifkhnmk@4ulsa^8$+rrq65hqbg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if is_dev() else False
 
 ALLOWED_HOSTS = ['*']
 
